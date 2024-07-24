3c94c7ded30cf79b39609ade0d6b8aef4c54fbec
Victor Dev <152346903+V1ctorroff@users.noreply.github.com>
2024-07-24 15:13:50 -0300

    Update serverhost.js
    mudado a porta de interface e mudando o caminho do arquivo

--- serverhost.js
+++ serverhost.js
@@ -1,11 +1,13 @@
+// dê "npm install express" antes de rodar o script
+
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 1024;
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+  res.sendFile(__dirname + '/site.html');
 });
 
 app.listen(port, '0.0.0.0', () => {
